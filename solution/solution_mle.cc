975d947ed142048e22c8cac13050f62d8c610f53
thak1411 <thak1411@gmail.com>
2022-07-18 20:38:53 +0900

    Update: mle solution

--- solution/solution_mle.cc
+++ solution/solution_mle.cc
@@ -3,8 +3,10 @@
 int main() {
     int a, b, c;
     scanf("%d%d%d", &a, &b, &c);
+    std::vector < std::vector < int > > w;
     for (volatile int i = 10000; i--; ) {
         volatile std::vector < int > v(10101010);
+        w.push_back(v);
     }
     printf("%d\n", a + b + c);
     return 0;
